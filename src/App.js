d9bc975ed672531be594b5bcda26219ad16b853f
beatrice2256 <beatrice2256mayle@outlook.com>
2024-08-31 17:22:46 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './components/Header';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from './components/ExpenseList';
+import Receipt from './components/Receipt';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="container">
+        <ExpenseForm />
+        <ExpenseList />
+        <Receipt />
+      </div>
+    </div>
+  );
+}
+
+export default App;
